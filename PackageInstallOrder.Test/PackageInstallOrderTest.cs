7830f59b11c3b41bb4ca2edf7c9d139461cf1226
FergyMC <snobord4life@gmail.com>
2017-05-09 17:55:29 -0600

    Began start for unit tests, will be adding unit next

---
+++ PackageInstallOrder.Test/PackageInstallOrderTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PackageInstallOrder.Test
+{
+    [TestClass]
+    public class PackageInstallOrderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PackageInstallOrder packageInstallOrder = new PackageInstallOrder();
+
+            string[] packages = { "KittenService: CamelCaser", "CamelCaser: " };
+
+            string expectedResult = "CamelCaser, KittenService";
+
+            string result = packageInstallOrder.GetPackageOrder(packages);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ PackageInstallOrder.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PackageInstallOrder.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PackageInstallOrder.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("249dd8e3-e633-40ca-90e7-3e50be574c01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
