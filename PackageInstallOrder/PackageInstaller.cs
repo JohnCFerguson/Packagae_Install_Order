b8b47fec243ea3a6a459d2ce2be49453439ab253
FergyMC <snobord4life@gmail.com>
2017-05-09 18:55:31 -0600

    started adding loops to check for dependencies

--- PackageInstallOrder/PackageInstaller.cs
+++ PackageInstallOrder/PackageInstaller.cs
@@ -12,11 +12,26 @@ namespace PackageInstallOrder
         {
             string installOrder = "";
 
+            for(int i = 0; i < packages.Length; i++)
+            {
+                char[] tmpPackage = packages[i].ToCharArray();
+                string tmpString = "";
 
+                int j = 0;
+                while (!tmpPackage[j].Equals(' '))
+                {
+                    j++;
+                }
+                j++;
+                while (j < tmpPackage.Length)
+                {
+                    tmpString += tmpPackage[j];
+                    j++;
+                }
+                installOrder += tmpString;
+            }
 
             return installOrder;
-
-
         }
     }
 }
